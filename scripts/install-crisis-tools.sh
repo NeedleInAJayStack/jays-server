30999eb885be9c530e3ba85ae0cb36c5a951ed16
Jay Herron <NeedleInAJayStack@protonmail.com>
2024-05-05 13:26:09 -0600

    feature: Adds crisis tools install script

---
+++ scripts/install-crisis-tools.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Set up crisis tools: https://www.brendangregg.com/blog/2024-03-24/linux-crisis-tools.html
+sudo apt-get install -y \
+  procps \
+  util-linux \
+  sysstat \
+  iproute2 \
+  numactl \
+  tcpdump \
+  linux-tools-common \
+  bpfcc-tools \
+  bpftrace \
+  trace-cmd \
+  nicstat \
+  ethtool \
+  tiptop \
+  cpuid \
+  msr-tools
