b0317819d5361f1e3ada3dd457473913455f5369
Jay Herron <NeedleInAJayStack@protonmail.com>
2024-06-20 23:42:29 -0600

    feat: Adds ec2 public IP

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -5,9 +5,6 @@ variable "cloudflare_api_token" {}
 variable "jaysdesktop_ip" {
   default = "76.23.30.196"
 }
-variable "jaysserver_ip" {
-  default = "18.236.68.87"
-}
 
 data "cloudflare_zone" "jayherron_org" {
   name = "jayherron.org"
@@ -18,28 +15,28 @@ resource "cloudflare_record" "a_bitwarden" {
   zone_id = data.cloudflare_zone.jayherron_org.id
   name    = "bitwarden"
   type    = "A"
-  value   = var.jaysserver_ip
+  value   = aws_eip.jays_server.public_ip
   proxied = true
 }
 resource "cloudflare_record" "a_cal" {
   zone_id = data.cloudflare_zone.jayherron_org.id
   name    = "cal"
   type    = "A"
-  value   = var.jaysserver_ip
+  value   = aws_eip.jays_server.public_ip
   proxied = true
 }
 resource "cloudflare_record" "a_data" {
   zone_id = data.cloudflare_zone.jayherron_org.id
   name    = "data"
   type    = "A"
-  value   = var.jaysserver_ip
+  value   = aws_eip.jays_server.public_ip
   proxied = true
 }
 resource "cloudflare_record" "a_grafana" {
   zone_id = data.cloudflare_zone.jayherron_org.id
   name    = "grafana"
   type    = "A"
-  value   = var.jaysserver_ip
+  value   = aws_eip.jays_server.public_ip
   proxied = true
 }
 resource "cloudflare_record" "a_home" {
@@ -53,7 +50,7 @@ resource "cloudflare_record" "a_jayherron_org" {
   zone_id = data.cloudflare_zone.jayherron_org.id
   name    = "jayherron.org"
   type    = "A"
-  value   = var.jaysserver_ip
+  value   = aws_eip.jays_server.public_ip
   proxied = true
 }
 resource "cloudflare_record" "a_nextcloud" {
@@ -74,7 +71,7 @@ resource "cloudflare_record" "a_recipes" {
   zone_id = data.cloudflare_zone.jayherron_org.id
   name    = "recipes"
   type    = "A"
-  value   = var.jaysserver_ip
+  value   = aws_eip.jays_server.public_ip
   proxied = true
 }
 resource "cloudflare_record" "a_superset" {
@@ -95,21 +92,21 @@ resource "cloudflare_record" "a_trello_bot" {
   zone_id = data.cloudflare_zone.jayherron_org.id
   name    = "trello-bot"
   type    = "A"
-  value   = var.jaysserver_ip
+  value   = aws_eip.jays_server.public_ip
   proxied = true
 }
 resource "cloudflare_record" "a_utility_api" {
   zone_id = data.cloudflare_zone.jayherron_org.id
   name    = "utility-api"
   type    = "A"
-  value   = var.jaysserver_ip
+  value   = aws_eip.jays_server.public_ip
   proxied = true
 }
 resource "cloudflare_record" "a_www" {
   zone_id = data.cloudflare_zone.jayherron_org.id
   name    = "www"
   type    = "A"
-  value   = var.jaysserver_ip
+  value   = aws_eip.jays_server.public_ip
   proxied = false
 }
 
