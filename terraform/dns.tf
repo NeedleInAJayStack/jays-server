8a9a38ba062e923b69ac53ed1df5dcadd9b291f1
Jay Herron <needleinajaystack@protonmail.com>
2024-12-08 19:14:59 -0700

    feat: Redirect from jayherron.org to herron.dev

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -22,25 +22,26 @@ data "cloudflare_zone" "jay_herron_com" {
 # jayherron.org
 
 resource "cloudflare_ruleset" "jayherron_org" {
-  zone_id     = data.cloudflare_zone.jayherron_org.id
-  name        = "default"
-  description = "Redirect to herron.dev"
-  kind        = "zone"
-  phase       = "http_request_dynamic_redirect"
+  zone_id = data.cloudflare_zone.jayherron_org.id
+  name    = "default"
+  kind    = "zone"
+  phase   = "http_request_dynamic_redirect"
 
   rules {
-    action = "redirect"
+    action      = "redirect"
+    description = "Redirect to herron.dev"
+    enabled     = true
+    expression  = "(http.request.full_uri wildcard r\"https://*.jayherron.org*\")"
+
     action_parameters {
       from_value {
-        status_code = 301
+        preserve_query_string = true
+        status_code           = 301
         target_url {
-          expression = "concat(\"https://\", wildcard_replace(http.host, \"*.jayherron.org\", \"$${1}.herron.dev\"), http.request.uri.path)"
+          expression = "wildcard_replace(http.request.full_uri, r\"https://*.jayherron.org*\", r\"https://$${1}.herron.dev$${2}\")"
         }
-        preserve_query_string = false
       }
     }
-    expression  = "(ends_with(http.host, \"jayherron.org\"))"
-    enabled     = false
   }
 }
 
@@ -49,99 +50,99 @@ resource "cloudflare_record" "a_bitwarden" {
   zone_id = data.cloudflare_zone.jayherron_org.id
   name    = "bitwarden"
   type    = "A"
-  value   = var.jaysdesktop_ip
+  content = var.jaysdesktop_ip
   proxied = true
 }
 resource "cloudflare_record" "a_cal" {
   zone_id = data.cloudflare_zone.jayherron_org.id
   name    = "cal"
   type    = "A"
-  value   = var.jaysdesktop_ip
+  content = var.jaysdesktop_ip
   proxied = true
 }
 resource "cloudflare_record" "a_data" {
   zone_id = data.cloudflare_zone.jayherron_org.id
   name    = "data"
   type    = "A"
-  value   = var.jaysdesktop_ip
+  content = var.jaysdesktop_ip
   proxied = true
 }
 resource "cloudflare_record" "a_grafana" {
   zone_id = data.cloudflare_zone.jayherron_org.id
   name    = "grafana"
   type    = "A"
-  value   = var.jaysdesktop_ip
+  content = var.jaysdesktop_ip
   proxied = true
 }
 resource "cloudflare_record" "a_home" {
   zone_id = data.cloudflare_zone.jayherron_org.id
   name    = "home"
   type    = "A"
-  value   = var.jaysdesktop_ip
+  content = var.jaysdesktop_ip
   proxied = true
 }
 resource "cloudflare_record" "a_jayherron_org" {
   zone_id = data.cloudflare_zone.jayherron_org.id
   name    = "jayherron.org"
   type    = "A"
-  value   = var.jaysdesktop_ip
+  content = var.jaysdesktop_ip
   proxied = true
 }
 resource "cloudflare_record" "a_nextcloud" {
   zone_id = data.cloudflare_zone.jayherron_org.id
   name    = "nextcloud"
   type    = "A"
-  value   = var.jaysdesktop_ip
+  content = var.jaysdesktop_ip
   proxied = true
 }
 resource "cloudflare_record" "a_plex" {
   zone_id = data.cloudflare_zone.jayherron_org.id
   name    = "plex"
   type    = "A"
-  value   = var.jaysdesktop_ip
+  content = var.jaysdesktop_ip
   proxied = true
 }
 resource "cloudflare_record" "a_recipes" {
   zone_id = data.cloudflare_zone.jayherron_org.id
   name    = "recipes"
   type    = "A"
-  value   = var.jaysdesktop_ip
+  content = var.jaysdesktop_ip
   proxied = true
 }
 resource "cloudflare_record" "a_superset" {
   zone_id = data.cloudflare_zone.jayherron_org.id
   name    = "superset"
   type    = "A"
-  value   = var.jaysdesktop_ip
+  content = var.jaysdesktop_ip
   proxied = true
 }
 resource "cloudflare_record" "a_syncthing" {
   zone_id = data.cloudflare_zone.jayherron_org.id
   name    = "syncthing"
   type    = "A"
-  value   = var.jaysdesktop_ip
+  content = var.jaysdesktop_ip
   proxied = true
 }
 resource "cloudflare_record" "a_trello_bot" {
   zone_id = data.cloudflare_zone.jayherron_org.id
   name    = "trello-bot"
   type    = "A"
-  value   = var.jaysdesktop_ip
+  content = var.jaysdesktop_ip
   proxied = true
 }
 resource "cloudflare_record" "a_utility_api" {
   zone_id = data.cloudflare_zone.jayherron_org.id
   name    = "utility-api"
   type    = "A"
-  value   = var.jaysdesktop_ip
+  content = var.jaysdesktop_ip
   proxied = true
 }
 resource "cloudflare_record" "a_www" {
   zone_id = data.cloudflare_zone.jayherron_org.id
   name    = "www"
   type    = "A"
-  value   = var.jaysdesktop_ip
-  proxied = false
+  content = var.jaysdesktop_ip
+  proxied = true
 }
 
 # CNAME records
@@ -149,21 +150,21 @@ resource "cloudflare_record" "cname_protonmail" {
   zone_id = data.cloudflare_zone.jayherron_org.id
   name    = "protonmail._domainkey"
   type    = "CNAME"
-  value   = "protonmail.domainkey.dd7nxyrs4dk5xdzlpo4hrsfomsmpgn6bf2i34c6lki4lzau6p4aaa.domains.proton.ch"
+  content = "protonmail.domainkey.dd7nxyrs4dk5xdzlpo4hrsfomsmpgn6bf2i34c6lki4lzau6p4aaa.domains.proton.ch"
   proxied = false
 }
 resource "cloudflare_record" "cname_protonmail2" {
   zone_id = data.cloudflare_zone.jayherron_org.id
   name    = "protonmail2._domainkey"
   type    = "CNAME"
-  value   = "protonmail2.domainkey.dd7nxyrs4dk5xdzlpo4hrsfomsmpgn6bf2i34c6lki4lzau6p4aaa.domains.proton.ch"
+  content = "protonmail2.domainkey.dd7nxyrs4dk5xdzlpo4hrsfomsmpgn6bf2i34c6lki4lzau6p4aaa.domains.proton.ch"
   proxied = false
 }
 resource "cloudflare_record" "cname_protonmail3" {
   zone_id = data.cloudflare_zone.jayherron_org.id
   name    = "protonmail3._domainkey"
   type    = "CNAME"
-  value   = "protonmail3.domainkey.dd7nxyrs4dk5xdzlpo4hrsfomsmpgn6bf2i34c6lki4lzau6p4aaa.domains.proton.ch"
+  content = "protonmail3.domainkey.dd7nxyrs4dk5xdzlpo4hrsfomsmpgn6bf2i34c6lki4lzau6p4aaa.domains.proton.ch"
   proxied = false
 }
 
@@ -172,7 +173,7 @@ resource "cloudflare_record" "mx_mailsec_protonmail" {
   zone_id  = data.cloudflare_zone.jayherron_org.id
   name     = "jayherron.org"
   type     = "MX"
-  value    = "mailsec.protonmail.ch"
+  content  = "mailsec.protonmail.ch"
   priority = 20
   proxied  = false
 }
@@ -180,7 +181,7 @@ resource "cloudflare_record" "mx_mail_protonmail" {
   zone_id  = data.cloudflare_zone.jayherron_org.id
   name     = "jayherron.org"
   type     = "MX"
-  value    = "mail.protonmail.ch"
+  content  = "mail.protonmail.ch"
   priority = 10
   proxied  = false
 }
@@ -190,28 +191,28 @@ resource "cloudflare_record" "txt_dmarc" {
   zone_id = data.cloudflare_zone.jayherron_org.id
   name    = "_dmarc"
   type    = "TXT"
-  value   = "v=DMARC1; p=none; rua=mailto:address@yourdomain.com"
+  content = "v=DMARC1; p=none; rua=mailto:address@yourdomain.com"
   proxied = false
 }
 resource "cloudflare_record" "txt_jayherron_org_spf1" {
   zone_id = data.cloudflare_zone.jayherron_org.id
   name    = "jayherron.org"
   type    = "TXT"
-  value   = "v=spf1 include:_spf.protonmail.ch mx ~all"
+  content = "v=spf1 include:_spf.protonmail.ch mx ~all"
   proxied = false
 }
 resource "cloudflare_record" "txt_jayherron_org_protonmail_verification" {
   zone_id = data.cloudflare_zone.jayherron_org.id
   name    = "jayherron.org"
   type    = "TXT"
-  value   = "protonmail-verification=5af174d6e5925d374fa8550f6b434760510f8ff9"
+  content = "protonmail-verification=5af174d6e5925d374fa8550f6b434760510f8ff9"
   proxied = false
 }
 resource "cloudflare_record" "txt_protonmail_domainkey" {
   zone_id = data.cloudflare_zone.jayherron_org.id
   name    = "protonmail._domainkey"
   type    = "TXT"
-  value   = "v=DKIM1; k=rsa; p=MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCz+Uq5u7Hn/wPpmjoehbWsALNFWDH8pUXAC4oupv9n2N9GfJTztyflJ7Vj/mAIAVyC0+YJBwUg7jPPgfc6MZiE+jknxpJWwMf2pSDrcTge/xCNePPEDR0ub/e9H0RYVuqVF9bcfonszg5N9JyM3YaTLJxe1l+PjFUzwG9ats+MAwIDAQAB"
+  content = "v=DKIM1; k=rsa; p=MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCz+Uq5u7Hn/wPpmjoehbWsALNFWDH8pUXAC4oupv9n2N9GfJTztyflJ7Vj/mAIAVyC0+YJBwUg7jPPgfc6MZiE+jknxpJWwMf2pSDrcTge/xCNePPEDR0ub/e9H0RYVuqVF9bcfonszg5N9JyM3YaTLJxe1l+PjFUzwG9ats+MAwIDAQAB"
   proxied = false
 }
 
@@ -223,42 +224,42 @@ resource "cloudflare_record" "herron_dev_a_root" {
   zone_id = data.cloudflare_zone.herron_dev.id
   name    = "herron.dev"
   type    = "A"
-  value   = var.jaysdesktop_ip
+  content = var.jaysdesktop_ip
   proxied = true
 }
 resource "cloudflare_record" "herron_dev_a_wildcard" {
   zone_id = data.cloudflare_zone.herron_dev.id
   name    = "*"
   type    = "A"
-  value   = var.jaysdesktop_ip
+  content = var.jaysdesktop_ip
   proxied = true
 }
 resource "cloudflare_record" "herron_dev_cname_protonmail" {
   zone_id = data.cloudflare_zone.herron_dev.id
   name    = "protonmail._domainkey"
   type    = "CNAME"
-  value   = "protonmail.domainkey.dcgwlficuvageoro6lkemesa6dzrqu2ypglv3njkxaje5rsjk23pq.domains.proton.ch."
+  content = "protonmail.domainkey.dcgwlficuvageoro6lkemesa6dzrqu2ypglv3njkxaje5rsjk23pq.domains.proton.ch."
   proxied = false
 }
 resource "cloudflare_record" "herron_dev_cname_protonmail2" {
   zone_id = data.cloudflare_zone.herron_dev.id
   name    = "protonmail2._domainkey"
   type    = "CNAME"
-  value   = "protonmail2.domainkey.dcgwlficuvageoro6lkemesa6dzrqu2ypglv3njkxaje5rsjk23pq.domains.proton.ch."
+  content = "protonmail2.domainkey.dcgwlficuvageoro6lkemesa6dzrqu2ypglv3njkxaje5rsjk23pq.domains.proton.ch."
   proxied = false
 }
 resource "cloudflare_record" "herron_dev_cname_protonmail3" {
   zone_id = data.cloudflare_zone.herron_dev.id
   name    = "protonmail3._domainkey"
   type    = "CNAME"
-  value   = "protonmail3.domainkey.dcgwlficuvageoro6lkemesa6dzrqu2ypglv3njkxaje5rsjk23pq.domains.proton.ch."
+  content = "protonmail3.domainkey.dcgwlficuvageoro6lkemesa6dzrqu2ypglv3njkxaje5rsjk23pq.domains.proton.ch."
   proxied = false
 }
 resource "cloudflare_record" "herron_dev_mx_mail_protonmail" {
   zone_id  = data.cloudflare_zone.herron_dev.id
   name     = "herron.dev"
   type     = "MX"
-  value    = "mail.protonmail.ch"
+  content  = "mail.protonmail.ch"
   priority = 10
   proxied  = false
 }
@@ -266,7 +267,7 @@ resource "cloudflare_record" "herron_dev_mx_mailsec_protonmail" {
   zone_id  = data.cloudflare_zone.herron_dev.id
   name     = "herron.dev"
   type     = "MX"
-  value    = "mailsec.protonmail.ch"
+  content  = "mailsec.protonmail.ch"
   priority = 20
   proxied  = false
 }
@@ -274,20 +275,20 @@ resource "cloudflare_record" "herron_dev_txt_protonmail_verification" {
   zone_id = data.cloudflare_zone.herron_dev.id
   name    = "herron.dev"
   type    = "TXT"
-  value   = "protonmail-verification=1fea0a3fe95573f3dd296b09d692b8c615faea75"
+  content = "protonmail-verification=1fea0a3fe95573f3dd296b09d692b8c615faea75"
   proxied = false
 }
 resource "cloudflare_record" "herron_dev_txt_dmarc" {
   zone_id = data.cloudflare_zone.herron_dev.id
   name    = "_dmarc"
   type    = "TXT"
-  value   = "v=DMARC1; p=quarantine"
+  content = "v=DMARC1; p=quarantine"
   proxied = false
 }
 resource "cloudflare_record" "herron_dev_txt_spf1" {
   zone_id = data.cloudflare_zone.herron_dev.id
   name    = "herron.dev"
   type    = "TXT"
-  value   = "v=spf1 include:_spf.protonmail.ch ~all"
+  content = "v=spf1 include:_spf.protonmail.ch ~all"
   proxied = false
 }
