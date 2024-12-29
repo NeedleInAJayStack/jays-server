7ff95d2a4450245f43e61cf1c680ac4654c41dfa
Jay Herron <needleinajaystack@protonmail.com>
2024-12-28 17:30:39 -0700

    fix: Renames particle database

--- scripts/db-backup.sh
+++ scripts/db-backup.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 pg_dump -Ft -h localhost -U postgres -f ~/Data/db/health.tar health
-pg_dump -Ft -h localhost -U postgres -f ~/Data/db/particle.tar particle
+pg_dump -Ft -h localhost -U postgres -f ~/Data/db/timeseries_api.tar timeseries_api
