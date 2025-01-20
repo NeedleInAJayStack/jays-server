8d7d6073115339f8f71cc387fa920047479ea14d
Jay Herron <NeedleInAJayStack@protonmail.com>
2025-01-20 00:55:47 -0700

    feat: Adds terraform for homeassistant alexa
    This adds an AWS lambda function for Alexa HomeAssistant integration.

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -4,7 +4,19 @@ terraform {
       source  = "cloudflare/cloudflare"
       version = "~> 4.0"
     }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    archive = {
+      source  = "hashicorp/archive"
+      version = "~> 2.0"
+    }
   }
+}
+
+provider "aws" {
+  region = "us-east-1"
 }
 
 terraform {
