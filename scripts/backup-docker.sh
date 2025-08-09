15891b755129ee734bee59d6198a276849cff7d0
Jay Herron <needleinajaystack@protonmail.com>
2025-08-09 13:35:45 -0600

    fix: Improves backup scripts
    This adds a delete flag, avoids permissions errors, improves modify time tracking, and improves documentation.

--- scripts/backup-docker.sh
+++ scripts/backup-docker.sh
@@ -1,20 +1,24 @@
 #! /bin/bash
 
 # Copies each directory in `~/Data/docker/` to a directory in `~/backup/`.
-# The backup directory is synchronized automatically via Syncthing with the remote backup.
+# It should be run on a scheduled cadence via the root cron to avoid permissions issues.
+# 
+# The backup directory is locally backed up and versioned to separate disk using duplicity (Ubuntu backup), and
+# synchronized automatically via Syncthing with the remote backup.
 # This approach was chosen over zip archives to reduce network traffic. It allows Syncthing to perform small incremental
 # syncing instead of many transfers of large archives that may have minimal changes.
 
 set -euo pipefail
 
-docker_dir=~/Data/docker/
-destination_dir=~/backup
+docker_dir=/home/jay/Data/docker/
+destination_dir=/home/jay/backup/docker
+owner=jay
 
 echo "Starting backup of $docker_dir"
 cd $docker_dir
 for dir in */; do
   destination=$destination_dir/$dir
   echo "Backing up $dir to $destination"
-  rsync -r "$dir" "$destination_dir/$dir"
+  rsync --atimes --chown=$owner:$owner --delete --recursive --times "$dir" "$destination_dir/$dir"
 done;
 echo "Backup complete"
--- scripts/backup-kubernetes.sh
+++ scripts/backup-kubernetes.sh
@@ -1,20 +1,24 @@
 #! /bin/bash
 
 # Copies each directory in `/var/openebs/local/` to a directory in `~/backup/kubernetes/openebs/`.
-# The backup directory is synchronized automatically via Syncthing with the remote backup.
+# It should be run on a scheduled cadence via the root cron to avoid permissions issues.
+#
+# The backup directory is locally backed up and versioned to separate disk using duplicity (Ubuntu backup), and
+# synchronized automatically via Syncthing with the remote backup.
 # This approach was chosen over zip archives to reduce network traffic. It allows Syncthing to perform small incremental
 # syncing instead of many transfers of large archives that may have minimal changes.
 
 set -euo pipefail
 
 source_dir=/var/openebs/local/
-destination_dir=~/backup/kubernetes/openebs
+destination_dir=/home/jay/backup/kubernetes/openebs
+owner=jay
 
 echo "Starting backup of $source_dir"
 cd $source_dir
 for dir in */; do
   destination=$destination_dir/$dir
   echo "Backing up $dir to $destination"
-  rsync -r "$dir" "$destination_dir/$dir"
+  rsync --atimes --chown=$owner:$owner --delete --recursive --times "$dir" "$destination_dir/$dir"
 done;
 echo "Backup complete"
